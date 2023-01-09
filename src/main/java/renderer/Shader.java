815a9eee8edc64b9dce5cd83f1b7c7439a1998f2
Vasu Dalal <vdalal@gmail.com>
2023-01-08 19:05:00 -0800

    Merge pull request #3 from vdalal/lesson8-glsl-shaders
    added texture shader (lesson #9)

--- src/main/java/renderer/Shader.java
+++ src/main/java/renderer/Shader.java
@@ -172,4 +172,10 @@ public class Shader {
         glUniform1i(varLocation, val);
     }
 
+    public void uploadTexture(String varName, int slot) {
+        int varLocation = glGetUniformLocation(shaderProgramID, varName);
+        use();
+        glUniform1i(varLocation, slot);
+    }
+
 }
